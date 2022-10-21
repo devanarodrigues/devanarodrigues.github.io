e6478391212624b64bb614ee679a000a4be2c5a2
Ana Rodrigues <dev.rodriguesana@gmail.com>
2022-10-21 09:41:01 -0300

    exp js

---
+++ exp.js
@@ -0,0 +1,7 @@
+function expandir() {
+    var display = document.getElementById('show-details').style.display;
+    if(display == "block")
+        document.getElementById('show-details').style.display = 'none';
+    else
+        document.getElementById('show-details').style.display = 'block';
+}
