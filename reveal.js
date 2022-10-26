59da0ef2cd6ee556467f4b96c4fae9122da6e759
Ana Rodrigues <dev.rodriguesana@gmail.com>
2022-10-26 10:09:11 -0300

    reveal

---
+++ reveal.js
@@ -0,0 +1,23 @@
+const sr = ScrollReveal({ reset: true })
+
+sr.reveal(".right", {
+    origin: "right", 
+    distance: "300px", 
+    duration: 1500, 
+    delay: 50, 
+    reset: false 
+});
+sr.reveal(".left", {
+    origin: "left", 
+    distance: "300px", 
+    duration: 1500, 
+    delay: 60, 
+    reset: false 
+});
+sr.reveal(".top", {
+    origin: "top", 
+    distance: "300px", 
+    duration: 1500, 
+    delay: 100,
+    reset: false 
+});
